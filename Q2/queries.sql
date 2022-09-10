b07c947f356dd4a15fc64892ca6f56b678db29e3
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-10 23:59:56 +0300

    query 5 Q2

--- Q2/queries.sql
+++ Q2/queries.sql
@@ -17,3 +17,10 @@ GROUP BY dept_name;
 SELECT enrolled.course_name as course_name
 FROM majorsin, enrolled
 WHERE dept_name like ('BIF') and enrolled.student_id=majorsin.student_id;
+--5
+SELECT name
+FROM student
+WHERE student.id not in (
+    SELECT student_id
+    FROM enrolled
+    GROUP BY student_id);
