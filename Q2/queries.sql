bdfd46b2f0cd2be7379a1854dd69cade064445d8
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-11 11:03:25 +0300

    removed query Q2

--- Q2/queries.sql
+++ Q2/queries.sql
@@ -10,9 +10,7 @@ WHERE start_time=(
     order by start_time ASC 
     limit 1);
 --3
-SELECT  count(student_id), dept_name 
-FROM majorsin
-GROUP BY dept_name;
+-
 -- 4
 SELECT DISTINCT course_name
 FROM enrolled
